a3009009d7d0421f1c7f45797a1a11b11aec4f41
Rubber Duck Shobe <xdercrafti@gmail.com>
2019-11-09 17:57:13 +0100

    Added Razer Chroma support and made some other tiny changes

---
+++ OLDTVMiscPlugin/OLDTVResources.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace adVance
+{
+    class OLDTVResources : MonoBehaviour
+    {
+        //Currently unused - feel free to add something if you want to
+        private GamePlay gp;
+
+        void Awake()
+        {
+            StartCoroutine(GetRequired());
+        }
+
+        IEnumerator GetRequired()
+        {
+            yield return new WaitUntil(() => Resources.FindObjectsOfTypeAll<GamePlay>().Any());
+            gp = Resources.FindObjectsOfTypeAll<GamePlay>().FirstOrDefault();
+        }
+    }
+}
--- OLDTVMiscPlugin/Plugin.cs
+++ OLDTVMiscPlugin/Plugin.cs
@@ -8,18 +8,22 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using IllusionPlugin;
+using ColoreColor = Corale.Colore.Core.Color;
+using Corale.Colore.Core;
+using Corale.Colore.Razer.Keyboard;
 
 namespace adVance
 {
     public class Plugin : IPlugin
     {
         public string Name => "adVance";
-        public string Version => "0.0.1";
+        public string Version => "0.1.1";
+        public bool arePeripheralsWhite = false;
+        public int oldTargetFramerate;
 
         public void OnApplicationStart()
         {
             //Code to execute when the game starts
-
             SceneManager.activeSceneChanged += SceneManagerOnActiveSceneChanged;
             SceneManager.sceneLoaded += SceneManager_sceneLoaded;
 
@@ -70,10 +74,30 @@ namespace adVance
         {
             //Code to execute whenever a graphics update occurs
 
-            //Sets the game's maximum refresh rate to 300.
+            //Razer Chroma integration
+            //Set the colors of all peripherals to white if they aren't already set to white
+            if (arePeripheralsWhite == false)
+            {
+                Console.WriteLine("[" + Name + "] Set all Razer peripheral colors to white");
+                Chroma.Instance.SetAll(ColoreColor.White);
+                arePeripheralsWhite = true;
+            }
+            
+            Chroma.Instance.Keyboard[Key.Left] = ColoreColor.Green;
+            Chroma.Instance.Keyboard[Key.Right] = ColoreColor.Red;
+
+            //Set first LED strip of the mouse to red
+            Chroma.Instance.Mouse.SetLed(Corale.Colore.Razer.Mouse.Led.Strip1, ColoreColor.Red);
+
+            //Set second LED strip of the mouse to green
+            Chroma.Instance.Mouse.SetLed(Corale.Colore.Razer.Mouse.Led.Strip2, ColoreColor.Green);
+
+            //Custom refresh rate cap - Sets the game's maximum refresh rate to 300.
             if (Application.targetFrameRate != 300)
             {
+                oldTargetFramerate = Application.targetFrameRate;
                 Application.targetFrameRate = 300;
+                Console.WriteLine("[" + Name + "] Set maximum framerate to " + Application.targetFrameRate + ", old framerate was " + oldTargetFramerate);
             }
         }
 
